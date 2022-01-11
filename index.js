a5c01537d7a146a0fd62d86371e24c8edd89dfff
Matt Quinn <mjq0533@gmail.com>
2022-01-11 11:52:04 -0600

    commit tree 999a9b266a3c5c75fcd97c32d300f608fc937d64

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Write your code in this file!
-const currentUser = "Grace Hopper";
+const currentUser = "Matt"
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
